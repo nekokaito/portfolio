0c884d2e3eb1477751b4d372e79caf10a78df5ae
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-12-05 14:10:13 +0600

    some changes

--- src/home/Root.jsx
+++ src/home/Root.jsx
@@ -1,24 +1,23 @@
-
 import Home from "./Home";
 import Tabs from "../components/Tabs";
 
 
 const Root = () => {
      return (
-          <div className="font-ubuntu">
-               {/* Home Section */}
-               <div id="home-section">
-                    <Home />
-               </div>
+          <div className="font-ubuntu relative">
+               
+                    {/* Home Section */}
+                    <div id="home-section">
+                         <Home />
+                    </div>
 
-               {/* Main Content */}
-               <div className="container mx-auto">
-
-                    {/* <Outlet /> */}
-                    <h1 className="text-2xl md:text-3xl text-center my-10 ">Highlights</h1>
-                    <Tabs></Tabs>
+                    {/* Main Content */}
+                    <div className="container mx-auto">
+                         <h1 className="text-2xl md:text-3xl text-center my-10 ">Highlights</h1>
+                         <Tabs />
+                    </div>
                </div>
-          </div>
+          
      );
 };
 
